184b5093a6bad3d40493a2f1e916bec5e4dea2c2
juramento <juramentojs@gmail.com>
2023-03-11 11:51:09 +0000

    v0.1.3

--- dist/_constants.js
+++ dist/_constants.js
@@ -1,2 +1,2 @@
-"use strict";var U=Object.defineProperty;var c=Object.getOwnPropertyDescriptor;var e=Object.getOwnPropertyNames;var l=Object.prototype.hasOwnProperty;var m=(S,o)=>{for(var B in o)U(S,B,{get:o[B],enumerable:!0})},n=(S,o,B,b)=>{if(o&&typeof o=="object"||typeof o=="function")for(let t of e(o))!l.call(S,t)&&t!==B&&U(S,t,{get:()=>o[t],enumerable:!(b=c(o,t))||b.enumerable});return S};var p=S=>n(U({},"__esModule",{value:!0}),S);var s={};m(s,{UNSUBSCRIBE:()=>r});module.exports=p(s);var r=Symbol();0&&(module.exports={UNSUBSCRIBE});
+var o=Symbol();export{o as UNSUBSCRIBE};
 //# sourceMappingURL=_constants.js.map
--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
-import * as _constants from './_constants.js'
-const result = { ..._constants }
-export default result
+export * as _constants from './_constants.js'
